9c36ebe2c686f4c8456f7dee382fd258b18cbea1
Hayel <ivanforwork@yandex.ru>
2022-03-09 04:43:48 +0300

    Base models added.

---
+++ src/main/java/by/teachmeskills/calculator/model/operation/Operation.java
@@ -0,0 +1,16 @@
+package by.teachmeskills.calculator.model.operation;
+
+import lombok.NonNull;
+import lombok.Value;
+
+import java.io.Serializable;
+
+@Value
+public class Operation<T extends Number> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NonNull String operand;
+    @NonNull T primaryValue;
+    T secondaryValue;
+    @NonNull T result;
+}
